6436780f0df2ba872935c5bde9d9a063ed4ddaef
liacdev <jagoblacksky@gmail.com>
2022-07-21 15:57:24 -0500

    Display score on UI

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     // Config parameters
     [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 1;
-
+    [SerializeField] TextMeshProUGUI scoreText;
+    
     // State variables
     [SerializeField] int currentScore = 0;
+
+    private void Start() {
+        DisplayScore();
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -20,5 +27,12 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        DisplayScore();
     }
+
+    private void DisplayScore()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
 }
